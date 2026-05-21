a80faaa0d9bfc817da1a77946315ae782bb53268
Joe Milburn <jrmilburn@outlook.com>
2026-05-21 19:47:20 +1000

    update base url in axiosConfig to address photo access bug

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const apiBase = process.env.REACT_APP_API_URL || 'http://localhost:5001';
 
 const axiosInstance = axios.create({
-  baseURL: "http://16.176.192.94:5001/",
+  baseURL: "http://localhost:5001",
   headers: { 'Content-Type': 'application/json' },
 });
 
