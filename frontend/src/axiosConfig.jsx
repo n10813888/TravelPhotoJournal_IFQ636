550833cd6a078c22d95b5c1b3ec33d5cfed97d44
Joe Milburn <jrmilburn@outlook.com>
2026-05-21 20:15:08 +1000

    update apiBase

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const apiBase = process.env.REACT_APP_API_URL || 'http://localhost:5001';
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001",
+  baseURL: apiBase,
   headers: { 'Content-Type': 'application/json' },
 });
 
